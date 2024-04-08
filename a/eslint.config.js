d89256fbc539e212fa2f7db2d8e338bc614c6046
freeroamer90 <agarwalvaibhav0211@gmail.com>
2024-04-09 01:00:33 +0530

    First Commit

---
+++ a/eslint.config.js
@@ -0,0 +1,4 @@
+import kalkiEslint from "@kalki/eslint-config-kalki/flat/library";
+export default [
+	...kalkiEslint
+];
---
+++ eslint-config-kalki/library.eslint.config.js
@@ -0,0 +1,11 @@
+
+
+export default [
+  {
+    rules: {
+      "no-var": [
+        "error",
+      ],
+    }
+  },
+]
